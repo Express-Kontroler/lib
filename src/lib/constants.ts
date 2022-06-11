1db1aa1bc31839c1d11f37c8dca627dfaa94f53c
T3NED <software@t3ned.com>
2022-06-11 05:27:55 +0100

    Add controller decorator

---
+++ src/lib/constants.ts
@@ -0,0 +1,3 @@
+export const CONTROLLER_METADATA = "controller";
+export const CONTROLLER_PREFIX_METADATA = "controller:prefix";
+export const CONTROLLER_VERSION_METADATA = "controller:version";
---
+++ src/lib/controller/Controller.ts
@@ -0,0 +1,40 @@
+import { CONTROLLER_METADATA, CONTROLLER_PREFIX_METADATA, CONTROLLER_VERSION_METADATA } from "../constants";
+
+/**
+ * A decorator that adds controller metadata to a class
+ * @param options The options to apply to the controller
+ */
+export function Controller(options: Partial<ControllerOptions> = {}): ClassDecorator {
+	if (!options?.prefix) options.prefix = "/";
+	if (!options?.version) options.version = "v1";
+
+	const prefix = Array.isArray(options.prefix) ? options.prefix : [options.prefix];
+	const version = Array.isArray(options.version) ? options.version : [options.version];
+
+	return (target: object) => {
+		Reflect.defineMetadata(CONTROLLER_METADATA, true, target);
+		Reflect.defineMetadata(CONTROLLER_PREFIX_METADATA, prefix, target);
+		Reflect.defineMetadata(CONTROLLER_VERSION_METADATA, version, target);
+	};
+}
+
+export interface ControllerOptions {
+	/**
+	 * The router prefix for the controller. Controller routes will append the prefix
+	 */
+	prefix: ControllerPrefix[] | ControllerPrefix;
+
+	/**
+	 * The versions accepted by the controller. Per-route versions can override controller versions
+	 */
+	version: ControllerVersion[] | ControllerVersion;
+}
+
+export type ControllerPrefix = `/${string}`;
+
+export type ControllerVersion =
+	| `v${ControllerVersionValue}`
+	| `${ControllerVersionValue}`
+	| ControllerVersionValue;
+
+export type ControllerVersionValue = number | `${number}.${number}`;
