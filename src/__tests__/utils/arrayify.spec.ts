5d0609079f6036caad45200f01e5a3da2840ad5e
T3NED <software@t3ned.com>
2022-06-15 23:03:02 +0100

    Add test for arrayify

---
+++ src/__tests__/utils/arrayify.spec.ts
@@ -0,0 +1,17 @@
+import { arrayify } from "../../lib/utils/arrayify";
+
+describe("utils.arrayify", () => {
+	test("given array value should return an array", () => {
+		const value = ["sample1", "sample2", "sample3"];
+		const result = arrayify(value);
+
+		expect(result).toEqual(["sample1", "sample2", "sample3"]);
+	});
+
+	test("given primitive value should return an array", () => {
+		const value = "sample1";
+		const result = arrayify(value);
+
+		expect(result).toEqual(["sample1"]);
+	});
+});
