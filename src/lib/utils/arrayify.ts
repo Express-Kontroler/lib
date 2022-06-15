064333d1040d63cb8b73394fb404d851632531a3
T3NED <software@t3ned.com>
2022-06-15 23:03:20 +0100

    Use generics for arrayify function

--- src/lib/utils/arrayify.ts
+++ src/lib/utils/arrayify.ts
@@ -2,4 +2,6 @@
  * Converts value to an array.
  * @param value The value to convert to an array
  */
-export const arrayify = (value: unknown) => (Array.isArray(value) ? value : [value]);
+export const arrayify = <T = unknown>(value: T | T[]): T[] => {
+	return Array.isArray(value) ? value : [value];
+};
