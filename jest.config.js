742b923d366a3dc43e22a9f92adea4a943141bc8
T3NED <software@t3ned.com>
2022-06-15 23:08:38 +0100

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
 	preset: "ts-jest",
 	testEnvironment: "node",
 	coverageDirectory: "coverage",
-	collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+	collectCoverageFrom: ["<rootDir>/src/lib/**/*.ts"],
 	testMatch: ["<rootDir>/src/__tests__/**/*.spec.ts"],
 };
