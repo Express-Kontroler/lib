35184328634b3e5ef27c6bd439a1e57e0641ea1e
T3NED <oss@t3ned.dev>
2022-07-03 18:46:52 +0100

    Add PreMiddleware and PostMiddleware decorators

--- src/lib/decorators/Middleware.ts
+++ src/lib/decorators/Middleware.ts
@@ -4,8 +4,8 @@ import { mergeArrayMetadata } from "../utils";
 
 /**
  * A decorator that adds middleware metadata to a controller method
- * @param fn
- * @param options
+ * @param fn The middleware function
+ * @param options The middleware options
  */
 export const Middleware = (fn: MiddlewareFn, options: Partial<MiddlewareOptions> = {}): MethodDecorator => {
 	if (!options?.order) options.order = "pre";
@@ -13,6 +13,22 @@ export const Middleware = (fn: MiddlewareFn, options: Partial<MiddlewareOptions>
 	return (_target: object, _key: string | symbol, descriptor: TypedPropertyDescriptor<any>) => {
 		mergeArrayMetadata(MIDDLEWARE_METADATA, [{ order: options.order, fn }], descriptor.value);
 	};
+};
+
+/**
+ * A decorator that adds pre middleware metadata to a controller method
+ * @param fn The middleware function
+ */
+export const PreMiddleware = (fn: MiddlewareFn): MethodDecorator => {
+	return Middleware(fn, { order: "pre" });
+};
+
+/**
+ * A decorator that adds post middleware metadata to a controller method
+ * @param fn The middleware function
+ */
+export const PostMiddleware = (fn: MiddlewareFn): MethodDecorator => {
+	return Middleware(fn, { order: "post" });
 };
 
 export interface MiddlewareOptions {
