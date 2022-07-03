47469fee282538bdf757c7034729f4b56cfce97c
T3NED <oss@t3ned.dev>
2022-07-03 18:47:10 +0100

    Add tests for PreMiddleware and PostMiddleware decorators

--- tests/decorators/middleware.test.ts
+++ tests/decorators/middleware.test.ts
@@ -1,4 +1,4 @@
-import { Middleware, MIDDLEWARE_METADATA } from "../../src";
+import { Middleware, PreMiddleware, PostMiddleware, MIDDLEWARE_METADATA } from "../../src";
 import { describe, test, expect } from "vitest";
 import "reflect-metadata";
 
@@ -84,5 +84,35 @@ describe("Middleware", () => {
 		const result = middlewareMetadata.fn();
 
 		expect(result).toBe("Test");
+	});
+
+	test("metadata for decorated middleware with pre method should RETURN pre order", () => {
+		class SampleController {
+			@PreMiddleware(() => {
+				return "Test";
+			})
+			public sampleRoute(): void {}
+		}
+
+		const controller = new SampleController();
+
+		const [middlewareMetadata] = Reflect.getMetadata(MIDDLEWARE_METADATA, controller.sampleRoute);
+
+		expect(middlewareMetadata).toStrictEqual({ fn: expect.any(Function), order: "pre" });
+	});
+
+	test("metadata for decorated middleware with post method should RETURN post order", () => {
+		class SampleController {
+			@PostMiddleware(() => {
+				return "Test";
+			})
+			public sampleRoute(): void {}
+		}
+
+		const controller = new SampleController();
+
+		const [middlewareMetadata] = Reflect.getMetadata(MIDDLEWARE_METADATA, controller.sampleRoute);
+
+		expect(middlewareMetadata).toStrictEqual({ fn: expect.any(Function), order: "post" });
 	});
 });
