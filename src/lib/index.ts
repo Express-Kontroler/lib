2f9f6e7967653797a8b2dfa20e14a02f9ed5a218
T3NED <oss@t3ned.dev>
2022-07-03 15:51:31 +0100

    Export utils

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,3 +5,5 @@ export * from "./constants";
 export * from "./decorators/Controller";
 export * from "./decorators/Middleware";
 export * from "./decorators/Route";
+
+export * as Utils from "./utils/arrayify";
