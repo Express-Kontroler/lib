07c8a915b4b445f5c20ce819c0aa42f01eaa9fe0
T3NED <oss@t3ned.dev>
2022-07-03 15:41:54 +0100

    Update tsconfig to include tests dir and remove jest config

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	preset: "ts-jest",
-	testEnvironment: "node",
-	coverageDirectory: "coverage",
-	collectCoverageFrom: ["<rootDir>/src/lib/**/*.ts"],
-	testMatch: ["<rootDir>/src/__tests__/**/*.spec.ts"],
-};
